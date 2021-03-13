6f9a0ff54efbf6ec7d8522f01a189e449767a37f
Tanner Gooding <tagoo@outlook.com>
2021-03-13 11:19:04 -0800

    Updating to v1.2.170

--- sources/Interop/Vulkan/vk_layer/VkChainType.cs
+++ sources/Interop/Vulkan/vk_layer/VkChainType.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from include/vulkan/vk_layer.h in the KhronosGroup/Vulkan-Headers repository for tag v1.2.162
+// Ported from include/vulkan/vk_layer.h in the KhronosGroup/Vulkan-Headers repository for tag v1.2.170
 // Original source is Copyright © 2015-2017 The Khronos Group Inc. Copyright © 2015-2017 Valve Corporation. Copyright © 2015-2017 LunarG, Inc.
 
 namespace TerraFX.Interop
