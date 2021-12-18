5bff0322388257db36205383f8094b0dddaffd46
Tanner Gooding <tagoo@outlook.com>
2021-12-18 11:41:28 -0800

    Updating headers to reference Vulkan v1.2.198

--- sources/Interop/Vulkan/Vulkan/vulkan/vulkan_ggp/Vulkan.cs
+++ sources/Interop/Vulkan/Vulkan/vulkan/vulkan_ggp/Vulkan.cs
@@ -1,6 +1,6 @@
 // Copyright © Tanner Gooding and Contributors. Licensed under the MIT License (MIT). See License.md in the repository root for more information.
 
-// Ported from include/vulkan/vulkan_ggp.h in the KhronosGroup/Vulkan-Headers repository for tag v1.2.189
+// Ported from include/vulkan/vulkan_ggp.h in the KhronosGroup/Vulkan-Headers repository for tag v1.2.198
 // Original source is Copyright © 2015-2021 The Khronos Group Inc.
 
 using System;
