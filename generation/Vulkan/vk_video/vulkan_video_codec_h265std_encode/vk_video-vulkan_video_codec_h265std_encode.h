4bdace58b0866e2aa3d95f0b26b4bb3d95d58f02
Tanner Gooding <tagoo@outlook.com>
2022-09-03 07:49:58 -0700

    Updating version to 1.3.224.1

--- generation/Vulkan/vk_video/vulkan_video_codec_h265std_encode/vk_video-vulkan_video_codec_h265std_encode.h
+++ generation/Vulkan/vk_video/vulkan_video_codec_h265std_encode/vk_video-vulkan_video_codec_h265std_encode.h
@@ -1,3 +1,4 @@
 #include <vulkan/vulkan.h>
+#include <vk_video/vulkan_video_codecs_common.h>
 #include <vk_video/vulkan_video_codec_h265std.h>
 #include <vk_video/vulkan_video_codec_h265std_encode.h>
